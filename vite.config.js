d51894fd88de6f0ae85bb180e81a37a866b87192
Rick Friedrich <rick.friedrich@dezem.de>
2021-11-17 00:41:35 +0100

    revert base url config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/lorawan-devices-with-public-js-decoder-page/"
+  plugins: [react()]
 })
